667eeaff8ff88c0eff31deea317221699007a18f
Алексей <121049033+alex6236@users.noreply.github.com>
2023-03-30 13:41:10 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# import requests
+
+TOKEN = '5755910136:AAE2VgCE3tQLXN2MIX-xC7qpuomlZuB0UyA'
+
+APY_KEY = 'DoJ0YrnhZlsewb1tWLhiOwMKP2LlmTlM'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'тугрик': 'MNT'
+}
